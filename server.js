0d1626b862e90ea8949a937ad69896ad6331f17f
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2021-11-18 00:11:47 +0530

    backend auth done

--- server.js
+++ server.js
@@ -28,14 +28,11 @@ app.use('/api/users', userRoutes);
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client", "build")))
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running');
-  });
-}
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 
 app.use(notFound);
 
